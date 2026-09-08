b6c8d22c40e15b0010289a019541e53b8c0aa589
alfhdalwzeer-lgtm <alfhdalwzeer@gmail.com>
2026-09-08 17:54:21 +0300

    Update message_model.dart

--- lib/lib/models/message_model.dart
+++ lib/lib/models/message_model.dart
@@ -1,1 +1,19 @@
+enum MessageStatus { sent, delivered, read }
 
+class Message {
+  final String id;
+  final String senderId;
+  final String text;
+  final String? mediaUrl;
+  final DateTime timestamp;
+  final MessageStatus status;
+
+  Message({
+    required this.id,
+    required this.senderId,
+    required this.text,
+    this.mediaUrl,
+    required this.timestamp,
+    this.status = MessageStatus.sent,
+  });
+}
